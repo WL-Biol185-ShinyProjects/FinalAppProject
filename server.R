4e3eb1c6e57cf22a7c6ab58c980cd7a1d34dd67c
Beau Merhige <merhiger20@mail.wlu.edu>
2019-10-29 12:29:54 -0400

    static mapping code (incomplete)

--- server.R
+++ server.R
@@ -12,3 +12,17 @@ require(maps)
 atlantic <- read.csv("https://github.com/merhiger20/FinalAppProject/blob/master/atlantic.csv")
 pacific <- read.csv("https://github.com/merhiger20/FinalAppProject/blob/master/pacific.csv")
 
+#making a static hurricane tracking map (for atlantic data)
+  
+atlantic_box <- make_bbox(lon = atlantic$Longitude, lat = atlantic$Latitude, f = 0.5)
+sq_map <- get_map(location = atlantic_box, maptype = "satellite", source = "google", zoom = 5)
+
+p <- ggmap(sq_map) +
+    geom_point(data = atlantic, mapping = aes(x = long, y = lat, color = Maximum_Wind)) +
+    geom_line(data = atlantic, mapping = aes(x= long, y =lat, color = Maximum_Wind)) +
+    scale_color_continuous(name = "Maximum Wind Speed (mph)", low = "yellow", high = "red") +
+  
+#need to fiugre out how to change title according to which hurricane was selected from drop-down box  
+    labs(title = "Tracking of Hurricane X")
+    x = "Longitude", y = "Latitude", 
+  NULL
