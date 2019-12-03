7fbd5bc4cfc55e99ae3e2a32fca66f9c2b3dea90
Matt Zeikel <zeikelm20@mail.wlu.edu>
2019-12-03 12:01:51 -0500

    my app, not sure how I can catch all the previous changes i made

---
+++ Matts_App.R
@@ -0,0 +1,94 @@
+
+# Import important packages 
+
+library(shiny)
+library(ggplot2)
+library(dbplyr)
+
+
+
+
+
+
+#adding Date and Month Column as Numeric values to "globalHurricane" dataframe  
+
+globalHurricane <- read.csv("globalHurricane.csv")
+
+globalHurricane$dateStr <- as.character(globalHurricane$Date)
+
+globalHurricane$dateStr <- substr(as.character(globalHurricane$Date), start = 1, stop = 4)
+
+globalHurricane$monthStr <- substr(as.character(globalHurricane$Date), start = 5, stop = 6)
+
+globalHurricane$Month <- as.numeric(globalHurricane$monthStr)
+
+globalHurricane$Year <- as.numeric(globalHurricane$dateStr)
+
+globalHurricane$dateStr <- NULL
+
+globalHurricane$monthStr <- NULL 
+
+#bulding dynamic bar graph 
+
+# make a data frame to hold months 
+
+dfHolder <- data.frame("Month" = c(1:12))
+
+
+
+
+
+
+server <- function(input, output) {
+  
+
+  
+  output$stormBar <- renderPlot({ 
+    
+    filteredData <- globalHurricane %>% filter(Status == input$stormType & Year == input$stormYear) 
+    
+    mergedfilteredData <- left_join(dfHolder, filteredData)
+    
+    summaryTable <- mergedfilteredData %>% group_by(Month) %>% summarise(count = n_distinct(ID, na.rm= TRUE)) 
+    
+    ggplot(summaryTable, aes(Month, count)) + geom_bar(stat = "identity") + coord_cartesian(ylim = c(0, 15)) + xlab("Month" ) + ylab("Number of Storms") + theme(axis.title.y = element_text(face="bold", size=14), 
+    axis.title.x = element_text(face = "bold", size = 14))
+  
+  })
+  
+}
+
+
+
+
+
+
+
+ui <- fluidPage(
+  
+  titlePanel("Storm Frequency Visualization"),
+  
+  
+  # Sidebar with a slider and selection inputs
+  
+  sidebarLayout(
+    
+    
+    
+    selectizeInput("stormType", label = "Select Storm Type", choices = unique(globalHurricane$Status),
+                   
+                   multiple = TRUE, selected = "TS"),
+    
+    sliderInput("stormYear", label = "Year", min = 1851, max = 2015 , value = 1851, animate = animationOptions(interval = 100, loop = FALSE))),
+    
+    
+    mainPanel(
+      plotOutput("stormBar")
+      
+    )
+    
+    
+  )
+
+
+runApp(shinyApp(ui = ui, server = server))
